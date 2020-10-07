55b71c0310cefb5d4857b22dd478e22556fc10f1
Ryan <ryan.michael.tate@gmail.com>
2020-10-07 11:50:58 -0700

    publish platform specific native module

--- native/install.js
+++ native/install.js
@@ -1,13 +1,13 @@
 const exec = require('child_process').exec
 
-exports.asyncExec = async (cmd) => {
+const asyncExec = async (cmd) => {
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
             throw error
         }
 
         if (stderr) {
-            console.log('error: ', stderr)
+            console.log('stderr: ', stderr)
         }
 
         return true
@@ -19,37 +19,21 @@ exports.asyncExec = async (cmd) => {
     try {
         switch (process.platform) {
             case 'darwin':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/darwin`
-                )
+                return await asyncExec(`npm i @fluvio/native-darwin -S`)
             case 'freebsd':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/linux`
-                )
+                return await asyncExec(`npm i @fluvio/native-linux -S`)
             case 'linux':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/linux`
-                )
+                return await asyncExec(`npm i @fluvio/native-linux -S`)
             case 'openbsd':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/linux`
-                )
+                return await asyncExec(`npm i @fluvio/native-linux -S`)
             case 'sunos':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/linux`
-                )
+                return await asyncExec(`npm i @fluvio/native-linux -S`)
             case 'win32':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/win`
-                )
+                return await asyncExec(`npm i @fluvio/native-win -S`)
             case 'cygwin':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/win`
-                )
+                return await asyncExec(`npm i @fluvio/native-win -S`)
             case 'netbsd':
-                return await exports.asyncExec(
-                    `npm install ${process.cwd()}/src/linux`
-                )
+                return await asyncExec(`npm i @fluvio/native-linux -S`)
             default:
                 throw new Error('Platform is not supported')
         }
--- native/publish.js
+++ native/publish.js
@@ -1,24 +1,38 @@
-const { asyncExec } = require('./install')
+const exec = require('child_process').exec
+
+const asyncExec = async (cmd) => {
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            throw error
+        }
+
+        if (stderr) {
+            console.log('stderr: ', stderr)
+        }
+
+        return true
+    })
+}
 
 // Dynamically publish native package;
 ;(async () => {
     switch (process.platform) {
         case 'darwin':
-            return await asyncExec('npm publish ./darwin')
+            return await asyncExec('npm publish ./src/darwin --access public')
         case 'freebsd':
-            return await asyncExec('npm publish ./linux')
+            return await asyncExec('npm publish ./src/linux --access public')
         case 'linux':
-            return await asyncExec('npm publish ./linux')
+            return await asyncExec('npm publish ./src/linux --access public')
         case 'openbsd':
-            return await asyncExec('npm publish ./linux')
+            return await asyncExec('npm publish ./src/linux --access public')
         case 'sunos':
-            return await asyncExec('npm publish ./linux')
+            return await asyncExec('npm publish ./src/linux --access public')
         case 'win32':
-            return await asyncExec('npm publish ./win')
+            return await asyncExec('npm publish ./src/win --access public')
         case 'cygwin':
-            return await asyncExec('npm publish ./win')
+            return await asyncExec('npm publish ./src/win --access public')
         case 'netbsd':
-            return await asyncExec('npm publish ./linux')
+            return await asyncExec('npm publish ./src/linux --access public')
         default:
             throw new Error('Platform is not supported')
     }
