daa82c9197ffee50e15fbeac67ce775c5867bb0f
simlay <simlay@users.noreply.github.com>
2020-12-24 13:20:14 -0800

    Added tsconfig.tests.json (#55)

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   modulePathIgnorePatterns:["./src/fluvio.spec.ts", "./dist",],
   testPathIgnorePatterns:["./src/fluvio.spec.ts"],
+  globals: {
+    'ts-jest': {
+      tsconfig: "tsconfig.tests.json"
+    }
+  }
 };
