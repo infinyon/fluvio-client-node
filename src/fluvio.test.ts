5186e4295f1f2287470572e875a28d7f48a70186
Esteban Borai <estebanborai@gmail.com>
2022-09-20 23:15:22 -0300

    chore: unit testing

--- src/fluvio.test.ts
+++ src/fluvio.test.ts
@@ -9,121 +9,121 @@ async function sleep(ms: number) {
     })
 }
 
-// describe('Fluvio Admin', () => {
-//     test('Connect FluvioAdmin', async () => {
-//         const fluvio = await Fluvio.connect()
-//         expect(fluvio).toBeInstanceOf(Fluvio)
-//         const admin = await fluvio.admin()
-//         expect(admin).toBeInstanceOf(FluvioAdmin)
-//     })
-// })
+describe('Fluvio Admin', () => {
+    test('Connect FluvioAdmin', async () => {
+        const fluvio = await Fluvio.connect()
+        expect(fluvio).toBeInstanceOf(Fluvio)
+        const admin = await fluvio.admin()
+        expect(admin).toBeInstanceOf(FluvioAdmin)
+    })
+})
 
-// describe('Fluvio Producer and Consume using AsyncIterator', () => {
-//     jest.setTimeout(100000) // 100 seconds
-//     let admin: FluvioAdmin
-//     let fluvio: Fluvio
-//     let topic: string
+describe('Fluvio Producer and Consume using AsyncIterator', () => {
+    jest.setTimeout(100000) // 100 seconds
+    let admin: FluvioAdmin
+    let fluvio: Fluvio
+    let topic: string
 
-//     beforeAll(async () => {
-//         topic = uuidV4()
-//         fluvio = await Fluvio.connect()
-//         admin = await fluvio.admin()
-//         console.log(`Creating topic ${topic}`)
-//         const new_topic = await admin.createTopic(topic)
-//         await sleep(topic_create_timeout)
-//     })
+    beforeAll(async () => {
+        topic = uuidV4()
+        fluvio = await Fluvio.connect()
+        admin = await fluvio.admin()
+        console.log(`Creating topic ${topic}`)
+        const new_topic = await admin.createTopic(topic)
+        await sleep(topic_create_timeout)
+    })
 
-//     afterAll(async () => {
-//         console.log(`Deleting topic ${topic}`)
-//         await admin.deleteTopic(topic)
-//         await sleep(topic_create_timeout)
-//     })
+    afterAll(async () => {
+        console.log(`Deleting topic ${topic}`)
+        await admin.deleteTopic(topic)
+        await sleep(topic_create_timeout)
+    })
 
-//     test('Send and Consume using iterator!', async () => {
-//         const producer = await fluvio.topicProducer(topic)
-//         const messages: string[] = []
+    test('Send and Consume using iterator!', async () => {
+        const producer = await fluvio.topicProducer(topic)
+        const messages: string[] = []
 
-//         const MAX_COUNT = 10
-//         console.log('send records')
-//         for (let i = 0; i < MAX_COUNT; i++) {
-//             const msg = `Message: ${i}`
-//             console.log(`sending message ${msg}`)
-//             let error
-//             for (let j = 0; j < 10; j++) {
-//                 try {
-//                     await producer.send(i.toString(), msg)
-//                     error = undefined
-//                     break
-//                 } catch (e) {
-//                     console.log(`ERROR error: ${e}`)
-//                     error = e
-//                 }
-//             }
-//             if (error !== undefined) {
-//                 throw error
-//             }
-//             expect(error).toBeUndefined()
-//             messages.push(msg)
-//             console.log(`sent message ${msg}`)
-//         }
-//         console.log(`Getting a consumer for ${topic}`)
+        const MAX_COUNT = 10
+        console.log('send records')
+        for (let i = 0; i < MAX_COUNT; i++) {
+            const msg = `Message: ${i}`
+            console.log(`sending message ${msg}`)
+            let error
+            for (let j = 0; j < 10; j++) {
+                try {
+                    await producer.send(i.toString(), msg)
+                    error = undefined
+                    break
+                } catch (e) {
+                    console.log(`ERROR error: ${e}`)
+                    error = e
+                }
+            }
+            if (error !== undefined) {
+                throw error
+            }
+            expect(error).toBeUndefined()
+            messages.push(msg)
+            console.log(`sent message ${msg}`)
+        }
+        console.log(`Getting a consumer for ${topic}`)
 
-//         const consumer = await fluvio.partitionConsumer(topic, 0)
-//         let counter = 0
-//         const stream = await consumer.createStream(Offset.FromBeginning())
-//         for await (const record of stream) {
-//             expect(record.valueString()).toEqual(`Message: ${counter}`)
-//             counter++
-//             if (counter >= MAX_COUNT) break
-//         }
-//         expect(counter).toEqual(MAX_COUNT)
-//     })
-// })
+        const consumer = await fluvio.partitionConsumer(topic, 0)
+        let counter = 0
+        const stream = await consumer.createStream(Offset.FromBeginning())
+        for await (const record of stream) {
+            expect(record.valueString()).toEqual(`Message: ${counter}`)
+            counter++
+            if (counter >= MAX_COUNT) break
+        }
+        expect(counter).toEqual(MAX_COUNT)
+    })
+})
 
-// describe('Fluvio Batch Producer', () => {
-//     jest.setTimeout(100000) // 100 seconds
-//     let admin: FluvioAdmin
-//     let fluvio: Fluvio
-//     let topic: string
+describe('Fluvio Batch Producer', () => {
+    jest.setTimeout(100000) // 100 seconds
+    let admin: FluvioAdmin
+    let fluvio: Fluvio
+    let topic: string
 
-//     beforeAll(async () => {
-//         topic = uuidV4()
-//         fluvio = await Fluvio.connect()
-//         admin = await fluvio.admin()
-//         console.log(`Creating topic ${topic}`)
-//         const new_topic = await admin.createTopic(topic)
-//         await sleep(topic_create_timeout)
-//     })
+    beforeAll(async () => {
+        topic = uuidV4()
+        fluvio = await Fluvio.connect()
+        admin = await fluvio.admin()
+        console.log(`Creating topic ${topic}`)
+        const new_topic = await admin.createTopic(topic)
+        await sleep(topic_create_timeout)
+    })
 
-//     afterAll(async () => {
-//         console.log(`Deleting topic ${topic}`)
-//         await admin.deleteTopic(topic)
-//         await sleep(topic_create_timeout)
-//     })
+    afterAll(async () => {
+        console.log(`Deleting topic ${topic}`)
+        await admin.deleteTopic(topic)
+        await sleep(topic_create_timeout)
+    })
 
-//     test('Send records using batch producer', async () => {
-//         const producer = await fluvio.topicProducer(topic)
+    test('Send records using batch producer', async () => {
+        const producer = await fluvio.topicProducer(topic)
 
-//         const MAX_COUNT = 10
-//         const records: KeyValue[] = []
-//         for (let i = 0; i < MAX_COUNT; i++) {
-//             const key = `${i}`
-//             const value = `This is record ${i}`
-//             const record: KeyValue = [key, value]
-//             records.push(record)
-//         }
-//         await producer.sendAll(records)
-//         const consumer = await fluvio.partitionConsumer(topic, 0)
-//         let counter = 0
-//         const stream = await consumer.createStream(Offset.FromBeginning())
-//         for await (const record of stream) {
-//             expect(record.valueString()).toEqual(`This is record ${counter}`)
-//             counter++
-//             if (counter >= MAX_COUNT) break
-//         }
-//         expect(counter).toEqual(MAX_COUNT)
-//     })
-// })
+        const MAX_COUNT = 10
+        const records: KeyValue[] = []
+        for (let i = 0; i < MAX_COUNT; i++) {
+            const key = `${i}`
+            const value = `This is record ${i}`
+            const record: KeyValue = [key, value]
+            records.push(record)
+        }
+        await producer.sendAll(records)
+        const consumer = await fluvio.partitionConsumer(topic, 0)
+        let counter = 0
+        const stream = await consumer.createStream(Offset.FromBeginning())
+        for await (const record of stream) {
+            expect(record.valueString()).toEqual(`This is record ${counter}`)
+            counter++
+            if (counter >= MAX_COUNT) break
+        }
+        expect(counter).toEqual(MAX_COUNT)
+    })
+})
 
 describe('Configures a SmartModule with a Filter configuration', () => {
     jest.setTimeout(100000) // 100 seconds
@@ -159,16 +159,26 @@ describe('Configures a SmartModule with a Filter configuration', () => {
             smartmoduleData: wasmSmartModule.toString('base64'),
         });
 
-        console.log(serverLogs);
+        console.log('Sends every server log through the producer');
+        let count = 0;
+        for (let log of serverLogs) {
+            count++;
+            producer.send(count.toString(), JSON.stringify(log));
+        }
+
+        console.log('Sends every server log through the producer');
+        for await (const record of stream) {
+            console.log(record);
+        }
     })
 });
 
-// describe('MacOSCi', () => {
-//     test('', async () => {
-//         // The errors will either be ['Fluvio socket error'] or ['Fluvio config error']
-//         // Getting expect to work with either is annoying.
-//         let error = await expect(Fluvio.connect()).rejects.toEqual([
-//             'Fluvio config error: Config has no active profile',
-//         ])
-//     })
-// })
+describe('MacOSCi', () => {
+    test('', async () => {
+        // The errors will either be ['Fluvio socket error'] or ['Fluvio config error']
+        // Getting expect to work with either is annoying.
+        let error = await expect(Fluvio.connect()).rejects.toEqual([
+            'Fluvio config error: Config has no active profile',
+        ])
+    })
+})
