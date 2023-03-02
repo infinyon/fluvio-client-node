816f666ba570666f216e511e6e0d84a8ec97f6db
Sebastian Imlay <sebastian.imlay@gmail.com>
2023-03-02 12:51:08 -0500

    fluvio 0.16 updates and dependency bumps

--- src/error.rs
+++ src/error.rs
@@ -4,8 +4,14 @@ use node_bindgen::derive::node_bindgen;
 #[node_bindgen]
 pub struct FluvioErrorJS(String);
 
-impl From<fluvio::FluvioError> for FluvioErrorJS {
+impl From<FluvioError> for FluvioErrorJS {
     fn from(inner: FluvioError) -> Self {
+        Self(inner.to_string())
+    }
+}
+
+impl From<anyhow::Error> for FluvioErrorJS {
+    fn from(inner: anyhow::Error) -> Self {
         Self(inner.to_string())
     }
 }
