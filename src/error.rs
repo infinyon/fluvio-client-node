9bd09168661aae317d211f5df4987a896672f047
Sehyo Chang <sehyo@fluvio.io>
2023-04-20 18:49:33 -0700

    update api

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,17 @@
-use fluvio::FluvioError;
 use node_bindgen::derive::node_bindgen;
 
 #[node_bindgen]
 pub struct FluvioErrorJS(String);
 
-impl From<fluvio::FluvioError> for FluvioErrorJS {
-    fn from(inner: FluvioError) -> Self {
-        Self(inner.to_string())
+impl FluvioErrorJS {
+    pub fn new(inner: String) -> Self {
+        Self(inner)
+    }
+}
+
+impl From<anyhow::Error> for FluvioErrorJS {
+    fn from(error: anyhow::Error) -> Self {
+        Self(error.to_string())
     }
 }
 
