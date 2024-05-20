d68ebe19f9c6ba83bda2a86e9907a099f8939346
Alan Chen <alan@infinyon.com>
2024-05-20 08:41:04 -0700

    chore: update to fluvio 0.11.8

--- src/admin.rs
+++ src/admin.rs
@@ -370,7 +370,11 @@ impl JSValue<'_> for PartitionWrap {
             let id = must_property!("id", u32, js_obj);
             let replicas = must_property!("replicas", Vec<i32>, js_obj);
 
-            Ok(Self(PartitionMap { id, replicas }))
+            Ok(Self(PartitionMap {
+                id,
+                replicas,
+                ..Default::default()
+            }))
         } else {
             Err(NjError::Other("partition map must be json".to_owned()))
         }
